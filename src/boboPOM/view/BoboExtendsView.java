92ab2aab515927dc5f910d8e1f43b3830cb3941a
JeremieAstray <5490584810@163.com>
2014-03-05 22:07:20 +0800

    对BoboExtendsView作小小调整

--- src/boboPOM/view/BoboExtendsView.java
+++ src/boboPOM/view/BoboExtendsView.java
@@ -4,15 +4,28 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 /**
  * Created by Jeremie on 14-3-5.
  */
 public class BoboExtendsView extends Region {
 
-    private ArrayList<ImageView> whiteBoboExtendImage = new ArrayList<ImageView>();
+    private Vector<ImageView> whiteBoboExtendImage = new Vector<ImageView>();
 
     public BoboExtendsView() {
-
+        this.setVisible(true);
     }
+
+    public void addWhiteBoboExtend(ImageView imageView){
+        whiteBoboExtendImage.add(imageView);
+        this.getChildren().addAll(imageView);
+    }
+
+    public void removeWhiteBoboExtend(ImageView imageView){
+        whiteBoboExtendImage.remove(imageView);
+        this.getChildren().remove(imageView);
+    }
+
+
 }
